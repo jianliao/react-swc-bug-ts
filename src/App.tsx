55b67753862b91f7f4dc7ee1e128b1b581b22176
Jian Liao <jianliao@adobe.com>
2022-10-24 15:34:25 -0700

    Comment out tab example to isolate the problem

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
 import { SpRadio, SpRadioGroup } from "./components/SpRadio";
-import { SpTabs, SpTab, SpTabPanel } from "./components/SpTabs";
+// import { SpTabs, SpTab, SpTabPanel } from "./components/SpTabs";
 import { SpTheme } from "./components/Theme";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <SpRadio value="third">Option 3</SpRadio>
           <SpRadio value="fourth">Option 4</SpRadio>
         </SpRadioGroup>
-        <SpTabs selected="4" size="m">
+        {/* <SpTabs selected="4" size="m">
           <SpTab label="Tab 1" value="1"></SpTab>
           <SpTab label="Tab 2" value="2"></SpTab>
           <SpTab label="Tab 3" value="3"></SpTab>
@@ -24,7 +24,7 @@ function App() {
           <SpTabPanel value="2">Content for Tab 2</SpTabPanel>
           <SpTabPanel value="3">Content for Tab 3</SpTabPanel>
           <SpTabPanel value="4">Content for Tab 4</SpTabPanel>
-        </SpTabs>
+        </SpTabs> */}
       </div>
     </SpTheme>
   );
